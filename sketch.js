a714b0d3ac2e2d23d1f13bf25ca6026a6849b524
shza0399 <szha0399@uni.sydeny.edu.au>
2023-11-05 12:19:23 +1100

    i updated the sketch.js and style.css

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
+let img;
+
+function preload() {
+  img = loadImage("artwork.jpg"); // Load the image
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight); // Set the canvas size to the window size
+  img.resize(windowWidth, windowHeight); // Resize the image to fill the canvas
+  noStroke();
+  img.loadPixels();
+
+  let gridSize = 10; // Size of each square in the grid
+
+  for (let x = 0; x < width; x += gridSize) {
+    for (let y = 0; y < height; y += gridSize) {
+      let index = (x + y * img.width) * 4;
+      
+      let r = img.pixels[index];
+      let g = img.pixels[index + 1];
+      let b = img.pixels[index + 2];
+      
+      fill(r, g, b);
+      
+      // Draw a rectangle with a random width and height
+      let w = random(gridSize / 2, gridSize * 1.5);
+      let h = random(gridSize / 2, gridSize * 1.5);
+      rect(x, y, w, h);
+    }
+  }
 }
 
 function draw() {
-  background(220);
+  // No need to redraw the image in every frame
 }
